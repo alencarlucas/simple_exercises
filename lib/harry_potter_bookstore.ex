9c738435d5221bdfcf52eea54e678cbbab5a42dd
alencarlucas <alencarlucaas@gmail.com>
2020-10-15 03:33:50 -0300

    Add simple discounts

--- lib/harry_potter_bookstore.ex
+++ lib/harry_potter_bookstore.ex
@@ -6,4 +6,77 @@ defmodule HarryPotterBookstore do
   def full_price(shopping_cart, price) do
     (Enum.map(shopping_cart, fn {_livro, quantidade} -> quantidade end) |> Enum.sum()) * price
   end
+
+  def all_books(shopping_cart) do
+    shopping_cart
+    |> Enum.sort_by(fn {_, quantidade} -> quantidade end, :desc)
+    |> Enum.map(fn {livro, quantidade} -> List.duplicate(livro, quantidade) end)
+    |> List.flatten()
+  end
+
+  def count_group_books(all_books, group_length, group_count \\ 0) do
+    uniq_books = Enum.uniq(all_books)
+
+    if length(uniq_books) < group_length do
+      group_count
+    else
+      count_group_books(
+        all_books -- Enum.slice(uniq_books, 0..group_length-1),
+        group_length,
+        group_count + group_length
+      )
+    end
+  end
+
+  def two_copies(shopping_cart) do
+    count_group_books(
+      all_books(shopping_cart),
+      2
+    )
+  end
+
+  def discount_two_copies(shopping_cart, price) do
+    two_copies = two_copies(shopping_cart)
+    all_books = all_books(shopping_cart)
+    (two_copies * 0.95 + (length(all_books) - two_copies)) * price
+  end
+
+  def three_copies(shopping_cart) do
+    count_group_books(
+      all_books(shopping_cart),
+      3
+    )
+  end
+
+  def discount_three_copies(shopping_cart, price) do
+    three_copies = three_copies(shopping_cart)
+    all_books = all_books(shopping_cart)
+    (three_copies * 0.90 + (length(all_books) - three_copies)) * price
+  end
+
+  def four_copies(shopping_cart) do
+    count_group_books(
+      all_books(shopping_cart),
+      4
+    )
+  end
+
+  def discount_four_copies(shopping_cart, price) do
+    four_copies = four_copies(shopping_cart)
+    all_books = all_books(shopping_cart)
+    (four_copies * 0.85 + (length(all_books) - four_copies)) * price
+  end
+
+  def five_copies(shopping_cart) do
+    count_group_books(
+      all_books(shopping_cart),
+      5
+    )
+  end
+
+  def discount_five_copies(shopping_cart, price) do
+    five_copies = five_copies(shopping_cart)
+    all_books = all_books(shopping_cart)
+    (five_copies * 0.80 + (length(all_books) - five_copies)) * price
+  end
 end
--- test/harry_potter_bookstore_test.exs
+++ test/harry_potter_bookstore_test.exs
@@ -9,6 +9,22 @@ defmodule HarryPotterBookstoreTest do
              [1, 7, 5, 2]
   end
 
+  test "all books" do
+    shopping_cart = [{1, 1}]
+    assert HarryPotterBookstore.all_books(shopping_cart) == [1]
+
+    assert HarryPotterBookstore.all_books([{1, 1}, {7, 3}, {5, 5}, {2, 4}]) ==
+             [5, 5, 5, 5, 5, 2, 2, 2, 2, 7, 7, 7, 1]
+
+    shopping_cart = [{1, 1}, {2, 1}]
+    assert HarryPotterBookstore.all_books(shopping_cart) == [1, 2]
+  end
+
+  test "count group books" do
+    all_books = [1, 2]
+    assert HarryPotterBookstore.count_group_books(all_books, 2) == 2
+  end
+
   test "calculate the full price of a shopping cart" do
     shopping_cart = [{1, 1}]
     assert HarryPotterBookstore.full_price(shopping_cart, 8) == 8
@@ -46,5 +62,33 @@ defmodule HarryPotterBookstoreTest do
 
     shopping_cart = [{1, 3}, {2, 2}, {3, 2}, {4, 1}]
     assert HarryPotterBookstore.discount_two_copies(shopping_cart, 8) == 8 * 8 * 0.95
+  end
+
+  test "count number of three copies of different books in a shopping cart" do
+    shopping_cart = [{1, 1}]
+    assert HarryPotterBookstore.three_copies(shopping_cart) == 0
+
+    shopping_cart = [{1, 1}, {2, 1}, {3, 1}]
+    assert HarryPotterBookstore.three_copies(shopping_cart) == 3
+
+    shopping_cart = [{1, 3}, {2, 2}, {3, 2}]
+    assert HarryPotterBookstore.three_copies(shopping_cart) == 6
+
+    shopping_cart = [{1, 3}, {2, 2}, {3, 2}, {4, 1}]
+    assert HarryPotterBookstore.three_copies(shopping_cart) == 6
+  end
+
+  test "calculate 10% discount for three books" do
+    shopping_cart = [{1, 1}]
+    assert HarryPotterBookstore.discount_three_copies(shopping_cart, 8) == 8
+
+    shopping_cart = [{1, 1}, {2, 1}, {3, 1}]
+    assert HarryPotterBookstore.discount_three_copies(shopping_cart, 8) == 3 * 8 * 0.90
+
+    shopping_cart = [{1, 3}, {2, 2}, {3, 2}]
+    assert HarryPotterBookstore.discount_three_copies(shopping_cart, 8) == 8 + 6 * 8 * 0.90
+
+    shopping_cart = [{1, 3}, {2, 2}, {3, 2}, {4, 1}]
+    assert HarryPotterBookstore.discount_three_copies(shopping_cart, 8) == 6 * 8 * 0.90 + 8 * 2
   end
 end
